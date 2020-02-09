c19ae739e6e789a4b2cd5189b7d32abc5c8b94cf
Philippe Martin <contact@elol.fr>
2020-02-09 18:02:26 +0100

    More tests

--- controllers/kobuilder_controller_test.go
+++ controllers/kobuilder_controller_test.go
@@ -60,6 +60,7 @@ var _ = Describe("KoBuilder controller", func() {
 
 			// Create
 			Expect(k8sClient.Create(context.Background(), created)).Should(Succeed())
+			defer k8sClient.Delete(context.Background(), created)
 
 			By("Expecting configmap created with correct data")
 			Eventually(func() bool {
@@ -78,5 +79,197 @@ var _ = Describe("KoBuilder controller", func() {
 				return k8sClient.Get(context.Background(), jobKey, f)
 			}, timeout, interval).Should(BeNil())
 		})
+
+		It("KoBuilder status should be Unknown", func() {
+
+			key := types.NamespacedName{
+				Name:      "my-ko-builder",
+				Namespace: "my-ns",
+			}
+
+			created := &kov1alpha1.KoBuilder{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      key.Name,
+					Namespace: key.Namespace,
+				},
+				Spec: kov1alpha1.KoBuilderSpec{
+					Registry:       "user/ko-builder",
+					ServiceAccount: "account@project.com",
+					Repository:     "github/com/test/repo",
+					Checkout:       "1.2.3",
+					ConfigPath:     "/templates",
+				},
+			}
+
+			// Create
+			Expect(k8sClient.Create(context.Background(), created)).Should(Succeed())
+			defer k8sClient.Delete(context.Background(), created)
+
+			Eventually(func() bool {
+				f := &kov1alpha1.KoBuilder{}
+				return k8sClient.Get(context.Background(), key, f) == nil &&
+					f.Status.State == kov1alpha1.Unknown
+			}, timeout, interval).Should(BeTrue())
+		})
+
+		Context("The pod of job is active", func() {
+
+			It("KoBuilder status should be Deploying", func() {
+
+				key := types.NamespacedName{
+					Name:      "my-ko-builder",
+					Namespace: "my-ns",
+				}
+
+				jobKey := types.NamespacedName{
+					Name:      "my-ko-builder-job",
+					Namespace: "my-ns",
+				}
+
+				created := &kov1alpha1.KoBuilder{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      key.Name,
+						Namespace: key.Namespace,
+					},
+					Spec: kov1alpha1.KoBuilderSpec{
+						Registry:       "user/ko-builder",
+						ServiceAccount: "account@project.com",
+						Repository:     "github/com/test/repo",
+						Checkout:       "1.2.3",
+						ConfigPath:     "/templates",
+					},
+				}
+
+				// Create
+				Expect(k8sClient.Create(context.Background(), created)).Should(Succeed())
+				defer k8sClient.Delete(context.Background(), created)
+
+				By("Expecting job created")
+				Eventually(func() error {
+					f := &batchv1.Job{}
+					return k8sClient.Get(context.Background(), jobKey, f)
+				}, timeout, interval).Should(BeNil())
+
+				job := &batchv1.Job{}
+				k8sClient.Get(context.Background(), jobKey, job)
+				job.Status.Active = 1
+				job.Status.Failed = 0
+				job.Status.Succeeded = 0
+				k8sClient.Status().Update(context.Background(), job)
+
+				Eventually(func() bool {
+					f := &kov1alpha1.KoBuilder{}
+					return k8sClient.Get(context.Background(), key, f) == nil &&
+						f.Status.State == kov1alpha1.Deploying
+				}, timeout, interval).Should(BeTrue())
+			})
+		})
+
+		Context("The pod of job is succeeded", func() {
+
+			It("KoBuilder status should be Deployed", func() {
+
+				key := types.NamespacedName{
+					Name:      "my-ko-builder",
+					Namespace: "my-ns",
+				}
+
+				jobKey := types.NamespacedName{
+					Name:      "my-ko-builder-job",
+					Namespace: "my-ns",
+				}
+
+				created := &kov1alpha1.KoBuilder{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      key.Name,
+						Namespace: key.Namespace,
+					},
+					Spec: kov1alpha1.KoBuilderSpec{
+						Registry:       "user/ko-builder",
+						ServiceAccount: "account@project.com",
+						Repository:     "github/com/test/repo",
+						Checkout:       "1.2.3",
+						ConfigPath:     "/templates",
+					},
+				}
+
+				// Create
+				Expect(k8sClient.Create(context.Background(), created)).Should(Succeed())
+				defer k8sClient.Delete(context.Background(), created)
+
+				By("Expecting job created")
+				Eventually(func() error {
+					f := &batchv1.Job{}
+					return k8sClient.Get(context.Background(), jobKey, f)
+				}, timeout, interval).Should(BeNil())
+
+				job := &batchv1.Job{}
+				k8sClient.Get(context.Background(), jobKey, job)
+				job.Status.Succeeded = 1
+				job.Status.Failed = 0
+				job.Status.Active = 0
+				k8sClient.Status().Update(context.Background(), job)
+
+				Eventually(func() bool {
+					f := &kov1alpha1.KoBuilder{}
+					return k8sClient.Get(context.Background(), key, f) == nil &&
+						f.Status.State == kov1alpha1.Deployed
+				}, timeout, interval).Should(BeTrue())
+			})
+		})
+
+		Context("The pod of job is failed", func() {
+
+			It("KoBuilder status should be ErrorDeploying", func() {
+
+				key := types.NamespacedName{
+					Name:      "my-ko-builder",
+					Namespace: "my-ns",
+				}
+
+				jobKey := types.NamespacedName{
+					Name:      "my-ko-builder-job",
+					Namespace: "my-ns",
+				}
+
+				created := &kov1alpha1.KoBuilder{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      key.Name,
+						Namespace: key.Namespace,
+					},
+					Spec: kov1alpha1.KoBuilderSpec{
+						Registry:       "user/ko-builder",
+						ServiceAccount: "account@project.com",
+						Repository:     "github/com/test/repo",
+						Checkout:       "1.2.3",
+						ConfigPath:     "/templates",
+					},
+				}
+
+				// Create
+				Expect(k8sClient.Create(context.Background(), created)).Should(Succeed())
+				defer k8sClient.Delete(context.Background(), created)
+
+				By("Expecting job created")
+				Eventually(func() error {
+					f := &batchv1.Job{}
+					return k8sClient.Get(context.Background(), jobKey, f)
+				}, timeout, interval).Should(BeNil())
+
+				job := &batchv1.Job{}
+				k8sClient.Get(context.Background(), jobKey, job)
+				job.Status.Failed = 1
+				job.Status.Succeeded = 0
+				job.Status.Active = 0
+				k8sClient.Status().Update(context.Background(), job)
+
+				Eventually(func() bool {
+					f := &kov1alpha1.KoBuilder{}
+					return k8sClient.Get(context.Background(), key, f) == nil &&
+						f.Status.State == kov1alpha1.ErrorDeploying
+				}, timeout, interval).Should(BeTrue())
+			})
+		})
 	})
+
 })
